9c669333176f7192104d52882925049cca9cda04
Geetha <danda.saigeetha@wipro.com>
2025-02-05 05:09:27 +0000

    maven project

---
+++ src/test/java/com/example/demo_hello_world/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.demo_hello_world;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+       System.out.println("hello maven prject");
+    }
+}
